dc3faad46e7523196a2ea4310cd260b5b9d47499
Abhinesh Sharma <abhineshsharma612@gmail.com>
2025-11-19 18:23:24 +0530

    commit color

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,21 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	opts = { style = "moon" },
-    config = function()
-        vim.cmd.colorscheme("tokyonight")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = false, undercurl = false })
-        vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", {})
+  "folke/tokyonight.nvim",
+  lazy = false,
+  opts = { style = "moon" },
+  config = function()
+    vim.cmd.colorscheme("tokyonight")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = false, undercurl = false })
+    vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", {})
 
-        -- Set the color for line numbers
-        vim.cmd [[
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#D5FF6B", italic = true })
+
+    -- Set the color for line numbers
+    vim.cmd [[
                     highlight LineNr guifg=#FFFF00
                     highlight LineNrAbove guifg=#000000
                     highlight LineNrBelow guifg=#000000
-                ]]
+                    ]]
 
-    end,
+  end,
 }
